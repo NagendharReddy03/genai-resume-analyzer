4937a3c64343e19cd15e23612171d17083f66bbe
Nagendhar Reddy Pothula <nagendharreddypothula@Mac.lan>
2025-04-30 18:04:16 -0500

    All basics done

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,34 +1,39 @@
-# app/auth/forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     email = StringField(
-        "Email",
-        validators=[DataRequired(), Email(), Length(max=120)]
-    )
-    password = PasswordField(
-        "Password",
-        validators=[DataRequired(), Length(min=8)]
-    )
-    confirm_password = PasswordField(
-        "Confirm Password",
+        'Email',
         validators=[
-            DataRequired(),
-            EqualTo("password", message="Passwords must match.")
+            DataRequired(message="Please enter your email"),
+            Email(message="Must be a valid email address")
         ]
     )
-    submit = SubmitField("Register")
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(message="Please enter a password")]
+    )
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired(message="Please confirm your password"),
+            EqualTo('password', message='Passwords must match')
+        ]
+    )
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
     email = StringField(
-        "Email",
-        validators=[DataRequired(), Email(), Length(max=120)]
+        'Email',
+        validators=[
+            DataRequired(message="Please enter your email"),
+            Email(message="Must be a valid email address")
+        ]
     )
     password = PasswordField(
-        "Password",
-        validators=[DataRequired()]
+        'Password',
+        validators=[DataRequired(message="Please enter your password")]
     )
-    remember = BooleanField("Remember Me")
-    submit = SubmitField("Login")
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
